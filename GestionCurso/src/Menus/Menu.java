bf7541212bfea45900d2ade2dea8bb73c9b51860
alumno <alumno@aula01-15>
2016-12-19 19:51:06 +0100

    1

---
+++ GestionCurso/src/Menus/Menu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menus;
+
+import gestioncurso.Modulo;
+
+/**
+ *
+ * @author alumno
+ */
+public class Menu {
+    
+    public static void pintaMenu(){
+        System.out.println("*****************************");
+        System.out.println("1. Guardar Modulo");
+        System.out.println("2. Buscar Modulo");
+        System.out.println("3. Ver todos los modulos");
+        System.out.println("4. Salir");
+        System.out.println("");
+        System.out.println("Introduzca una opcion: ");
+        System.out.println("*****************************");
+    }
+    
+    
+    
+    
+    
+}
