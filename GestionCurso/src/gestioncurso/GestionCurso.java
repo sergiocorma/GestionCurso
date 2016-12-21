c1eeeae4528f10018037ce457d203f22f194517f
alumno <alumno@aula01-15>
2016-12-21 20:49:04 +0100

    version 5.0 sin terminar 1

--- GestionCurso/src/gestioncurso/GestionCurso.java
+++ GestionCurso/src/gestioncurso/GestionCurso.java
@@ -10,20 +10,64 @@ import java.util.Scanner;
 
 /**
  *
- * @author alumno
+ * @author Sergio Corma
  */
 public class GestionCurso {
-
+    
+    public Curso buscarCurso(Curso[] vCursos, String nombre){
+        Curso nuevo=null;
+        
+        for (Curso curso1:vCursos){
+            if (curso1.getNombre().equalsIgnoreCase(nombre)){
+                nuevo = curso1;
+            }
+        }
+        return nuevo;
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int opcion1=0;
+        Curso[] vCursos=new Curso[3];
+        Curso cursoActual;
+        String nombre;
+        //menu
+        
+        
+        
+        do{
+            Scanner leer=new Scanner(System.in);
+            Menu.pintaMenu();
+            opcion1=leer.nextInt();
+            switch(opcion1){
+                case 1:
+                    
+                    break;
+                case 2:
+                    System.out.println("Introduce el nombre del Curso: ");
+                    nombre=leer.next();
+                    cursoActual = buscarCurso();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("¡Opcion no valida!");
+                    break;
+                
+            }
+            
+        }while(opcion1!=4);
         
         
         
         
-        
-        Curso primeroDAW = new Curso("primero DAW");
+       
         
         int opcion;
         
@@ -33,17 +77,22 @@ public class GestionCurso {
             opcion=leer.nextInt();
             switch(opcion){
                 case 1:
-                    primeroDAW.insertarModulo();
+                    cursoActual.insertarModulo();
                     break;
                 case 2:
-                    primeroDAW.mostrarModulos();
+                    cursoActual.mostrarModulos();
                     break;
                 case 3:
-                    primeroDAW.consultarHorasTotales();
+                    cursoActual.consultarHorasTotales();
                     break;
                 case 4:
-                    System.out.println("ADIOS!");
+                    System.out.println("Introduce el nombre del modulo a eliminar: ");
+                    String nombre=leer.next();
+                    cursoActual.eliminarModulo(nombre);
                     
+                    break;
+                case 5:
+                    System.out.println("ADIOS!");
                     break;
                 default:
                     System.out.println("¡Opcion invalida!");
@@ -51,7 +100,7 @@ public class GestionCurso {
             }
                 
             
-            }while (opcion!=4);
+            }while (opcion!=5);
         
         
         
